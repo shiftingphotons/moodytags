230d0e5bde3c5377294517a6bab209e7b05bb10f
Dimitar Ralev <dralev@pm.me>
2020-06-17 15:58:19 +0300

    Add db_host env var

---
+++ spec/api/routes_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Api.routes do
+  it 'recognizes "GET /ping"' do
+    env   = Rack::MockRequest.env_for('/ping')
+    route = described_class.recognize(env)
+
+    expect(route.routable?).to be(true)
+
+    expect(route.path).to   eq('/ping')
+    expect(route.verb).to   eq('GET')
+    expect(route.params).to eq({})
+  end
+end
