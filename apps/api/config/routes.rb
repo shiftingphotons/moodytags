85e9585eb50452237bcccccd63978f1e3849d7b7
Dimitar Ralev <dralev@pm.me>
2020-09-28 11:49:32 +0300

    Add get user tags endpoint

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,6 +1,8 @@
 get '/ping', to: ->(env) { [200, {}, ['pong']] }
 
 # V1
+get '/api/v1/user/tags', to: 'user#get_tags'
+
 get '/api/v1/taggables', to: 'taggables#index'
 post '/api/v1/taggables', to: 'taggables#create'
 patch '/api/v1/taggables/:id', to: 'taggables#update'
