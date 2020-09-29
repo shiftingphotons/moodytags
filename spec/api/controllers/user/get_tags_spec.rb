1f6e4bff209b1d942afab14a4680ed0266aca34e
Dimitar Ralev <dralev@pm.me>
2020-09-29 10:49:41 +0300

    Fix specs

--- spec/api/controllers/user/get_tags_spec.rb
+++ spec/api/controllers/user/get_tags_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "API V1 get user tags" do
     tags_arr = JSON.parse(last_response.body)
     all_keys_present = tags_arr.all? {|t| t.keys == ["name", "tags", "order"]}
 
-    expect(all_keys).to be true
+    expect(all_keys_present).to be true
     expect(last_response.status).to be 200
   end
 end
